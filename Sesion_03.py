7ca4da5609353dd9eb893fb671b9364c9aaf1c24
sanchezmedina67 <is3030354@gmail.com>
2026-02-12 17:17:51 -0800

    Loop python basico

--- Sesion_03.py
+++ Sesion_03.py
@@ -19,3 +19,35 @@ for i in mi_lista:
     for i in mi_lista_2:
         print("Los dias de la semana son: ",i)
         print(f"Feliz(i)!")
+
+        #While loop
+        i = 0
+
+    while i < 5:
+        if i == 3:
+            continue
+        print(i)
+        if i ==4:
+            break
+
+        else:
+            print("i es ahora mayor o igual a 5")
+
+            # Practica 2 
+            # Dada la lista mi_lista_ = ["Lunes","martes","miercoles","jueves","viernes"]
+            # Imprime cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+            # Pista: Usa los dos tipos loop while y for en el mimo programa para lograrlo
+            # Resultado:
+            # Martes
+            # Miercoles
+            # Jueves
+            # Viernes
+            # Martes
+            # Miercoles
+            # Jueves
+            # Viernes
+            # Martes
+            # Mircoles
+            # Jueves
+            # Viernes
+             
